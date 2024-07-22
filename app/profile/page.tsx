b70445064bdbbacd6982bec00777159c441ae4c2
The-Professor99 <festusihechi99@gmail.com>
2024-07-22 21:27:31 +0100

    Add dummy profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,27 @@
+import { signOut } from '@/auth';
+import { SubmitButton } from '@/components/auth/submit-button';
+import { PowerIcon } from '@heroicons/react/24/outline';
+
+export default function () {
+  return (
+    <div className="h-screen flex items-center">
+      <div>
+        WIP | Your profile details will appear here
+        <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+          className="flex justify-center mt-4"
+        >
+          <div className="max-w-40">
+            <SubmitButton>
+              <PowerIcon className="w-6" />
+              <div>Sign Out</div>
+            </SubmitButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
