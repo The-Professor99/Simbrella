6b3e65828ee210f5ce553dbb363437b0120aeb84
The-Professor99 <festusihechi99@gmail.com>
2024-07-22 21:21:42 +0100

    Add error file

---
+++ app/error.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import ErrorUI from '@/components/layout/error';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return <ErrorUI resetAction={() => reset()} />;
+}
