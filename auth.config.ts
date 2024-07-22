8dace74a43be01310707193842b1593f396c522d
The-Professor99 <festusihechi99@gmail.com>
2024-07-23 00:21:47 +0100

    update routing behavior upon authorization

--- auth.config.ts
+++ auth.config.ts
@@ -10,10 +10,17 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnProfile = nextUrl.pathname.startsWith(routes.profile.path);
-      if (isOnProfile) {
-        if (isLoggedIn) return true;
+      const isOnLogin = nextUrl.pathname.startsWith(routes.login.path);
+      const isOnRegister = nextUrl.pathname.startsWith(routes.register.path);
+
+      if (isOnProfile && !isLoggedIn) {
         return false;
       }
+
+      if (isLoggedIn && (isOnLogin || isOnRegister)) {
+        return Response.redirect(new URL(routes.profile.path, nextUrl));
+      }
+
       return true;
     },
   },
